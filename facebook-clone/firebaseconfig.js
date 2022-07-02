a20455224ca2a515a228d7703fe089d8a1a01f03
Utkarsh <92447462+utkarsh07-co-in@users.noreply.github.com>
2022-07-03 00:15:45 +0530

    Add files via upload

---
+++ facebook-clone/firebaseconfig.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyATTWZJrzJken6EkM60B8JjDOM9Vm6lnMI",
+    authDomain: "utkarsh-raj-sinha.firebaseapp.com",
+    projectId: "utkarsh-raj-sinha",
+    storageBucket: "utkarsh-raj-sinha.appspot.com",
+    messagingSenderId: "417959145412",
+    appId: "1:417959145412:web:9ecb3dcf895ec05d41fe91",
+    measurementId: "G-G13T6YHXDN"
+  };
+  const cometChat = {
+    APP_ID: "213483cde51b3523",
+    AUTH_KEY: "d2e5b8bdd8d49f4be50770aaa969dc053f3c12b9",
+    REST_KEY: "v3.0",
+    REGION: "us"
+  }
+  
+  export { firebaseConfig, cometChat }
+  
